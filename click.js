99ee8146d0d0c6c984590316e937f724529fb06b
Mohammad Ismail <mohammad.ismail9190@gmail.com>
2020-08-21 14:50:29 +0300

    test

--- click.js
+++ click.js
@@ -1,8 +1,33 @@
+AFRAME.registerComponent('change-color-on-click', {
+    // Could use a schem to preserve the color! then simply change it on update
+    // if clicked?
+    init: function () {
+    let COLORS = [
+    'pink',
+    //'blue',
+    'yellow',
+    'red',
+    'peachpuff',
+    '#2EAFAC',
+    '#BAE'];
+    this.el.addEventListener('click', function (evt) {
+    let randomIndex = Math.floor(Math.random() * COLORS.length);
+    let newColor = COLORS[randomIndex];
+    this.setAttribute('color', newColor);
+    console.log('I was clicked at: ', evt.detail.intersection.point, "and my new color is: ", newColor);
+    });
+    }
+    });
+
+
+
+
+
+
 AFRAME.registerComponent("model", {
     init: function () {
     this.el.addEventListener("click", function (e) {
-    
-      console.log("I was clicked at: ");
+      console.log("I was clicked at: ", e.detail.intersection.point);
      });
     }
 });
